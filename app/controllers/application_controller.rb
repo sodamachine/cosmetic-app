91b5e08256bb28263ab4087f3a0a82f2d288a8cb
Stephanie <stephanie.ohn@gmail.com>
2021-04-02 14:48:54 -0400

    adds bcrypt gem, updater app controller for sessions, and adds passwords c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < Sinatra::Base
     configure do
         set(:views, 'app/views')
         set :public_folder, 'public'
+        enable :sessions #so that Sinatra can work with browser to store user info via cookie hash
+        set :session_secret, 'secret' #bc shotgun generates new server/request, browser now won't update cookie per new session
     end
 
     get('/') do
         @name = "test"
         erb :home
     end
-
-    
 
 end
--- db/migrate/02_create_users.rb
+++ db/migrate/02_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migrations
     def change
         create_table :users do |t|
             t.string :username
+            t.string :passwords
         end
     end
 end
