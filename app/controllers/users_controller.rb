b594252982a05dfbd448397757ce9889a76ad163
Stephanie <stephanie.ohn@gmail.com>
2021-04-02 15:39:33 -0400

    adds user sign up routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'users/signup'
+    end
+
+    post '/users' do
+        u = User.create(params[:user])
+        session[:user_id] = u.id
+        redirect '/users/#{@user.id}'
+    end
+
+    get '/users/:id' do
+        @user = User.find_by(id: params[:id])
+        erb :'users/show'
+    end
+
+    get '' do
+    end
+
+    get '' do
+    end
+
 end
