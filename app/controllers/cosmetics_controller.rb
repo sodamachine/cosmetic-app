dfe9e73d565d7bdbb98ffc283fe23c0fbcb5adca
Stephanie <stephanie.ohn@gmail.com>
2021-04-02 12:50:56 -0400

    sets up views

--- app/controllers/cosmetics_controller.rb
+++ app/controllers/cosmetics_controller.rb
@@ -1,1 +1,21 @@
 class CosmeticsController < ApplicationController
+
+    get '/cosmetics' do
+        @cosmetics = Cosmetic.require_all
+        erb :'cosmetics/index'
+    end
+
+    get '/cosmetics/new' do
+        erb :'cosmetics/new'
+    end
+
+    post '/cosmetics' do
+        @cosmetic = Cosmetic.create(name: params[:cosmetic][:name])
+        erb :'cosmetics/show'
+    end
+
+    get '/cosmetics/:id' do
+        erb :'cosmetics/show'
+    end
+
+end
