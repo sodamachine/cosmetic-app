4fe6d45ea47f418ae30ca389613f85cdfc3d2655
Stephanie <stephanie.ohn@gmail.com>
2021-04-02 12:55:49 -0400

    adds HTML form for post+get requests

--- db/migrate/01_create_cosmetics.rb
+++ db/migrate/01_create_cosmetics.rb
@@ -2,7 +2,7 @@ class CreateCosmetics < ActiveRecord::Migrations
     def change
         create_table :cosmetics do |t|
             t.string :name
-            t.string :quantity
+            t.string :brand
             t.integer :user_id
         end
     end
