f68849494e9f832e2e20ce070c64b0f3cb8a3142
Stephanie <stephanie.ohn@gmail.com>
2021-04-02 16:08:06 -0400

    adds layout.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
 
     get '/signup' do
+        if session[:user_id]
+            redirect "/users/#{session[:user_id]}"
+        end
         erb :'users/signup'
     end
 
     post '/users' do
         u = User.create(params[:user])
         session[:user_id] = u.id
-        redirect '/users/#{@user.id}'
+        redirect "/users/#{@user.id}"
     end
 
     get '/users/:id' do
@@ -15,10 +18,28 @@ class UsersController < ApplicationController
         erb :'users/show'
     end
 
-    get '' do
+    get '/login' do
+        if session[:user_id]
+            redirect "/users/#{session[:user_id]}"
+        end
+        erb :'users/login'
     end
 
-    get '' do
+    post '/login' do
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect "/users/#{user.id}"
+        else
+            erb :'users/login'
+        end
     end
+
+    get '/logout' do
+        session.clear
+        redirect '/login'
+    end
+
+
 
 end
